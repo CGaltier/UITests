44d6064361dbe9992571fdfdc0bc8b21f132206d
Christian <Christian@Christian-Asus>
2012-11-17 00:51:42 +0100

    9patches

--- UITests/src/com/cgaltier/uitests/UITests.java
+++ UITests/src/com/cgaltier/uitests/UITests.java
@@ -2,19 +2,30 @@ package com.cgaltier.uitests;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class UITests implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+	private Stage stage ;
 	
 	@Override
 	public void create() {		
@@ -27,18 +38,47 @@ public class UITests implements ApplicationListener {
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
+		//TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		TextureAtlas atlas;
+		atlas = new TextureAtlas(Gdx.files.internal("data/AtlasImage.atlas"));
+		//sprite = new Sprite(region);
+		sprite = atlas.createSprite("back320");
 		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
 		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
 		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		stage = new Stage ();
+		Gdx.input.setInputProcessor(stage);
+		
+		Table table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
+		
+		//TextureRegion upRegion = atlas.findRegion("button32");
+		//TextureRegion downRegion = atlas.findRegion("button32");	
+		//TextButtonStyle style = new TextButtonStyle();
+		BitmapFont buttonFont = new BitmapFont ();
+
+		//style.up = new TextureRegionDrawable (upRegion);
+		//style.down=new TextureRegionDrawable (downRegion);
+		//style.font = buttonFont;
+		NinePatch patch = atlas.createPatch("button32");
+		TextButtonStyle style = new TextButtonStyle ();
+		style.up = new NinePatchDrawable (patch);
+		style.down = new NinePatchDrawable (patch);
+		style.font = buttonFont;
+		
+		//TextButtonStyle style = new TextButtonStyle (patch,patch,patch,0,);// (patch, patch, patch, 0, 0, 0, 0, new BitmapFont(), new Color(0.3f, 0.2f, 0.8f, 1f), new Color(0, 0, 0, 1f), new Color(0, 0, 0, 1f));
+		TextButton button1 = new TextButton ("Button 1 - Button 1 - Button 1",style);
+		table.add(button1);
+		TextButton button2 = new TextButton ("Button 2 - Button 2", style);
+		table.add (button2);
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
 		texture.dispose();
+		stage.dispose();
 	}
 
 	@Override
@@ -50,10 +90,15 @@ public class UITests implements ApplicationListener {
 		batch.begin();
 		sprite.draw(batch);
 		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+		Table.drawDebug(stage);
 	}
 
 	@Override
-	public void resize(int width, int height) {
+	public void resize(int width, int height) 
+	{
+		stage.setViewport (width,height,true);
 	}
 
 	@Override
